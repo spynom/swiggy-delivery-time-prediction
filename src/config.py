b16ee95ebd9dcbdfb7a14c614e4ecc27518c105f
spynom <saurav24082000@gmail.com>
2025-04-06 19:47:45 +0530

    data cleaning stage added in dvc pipeline

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
+INTERIM_DATA_DIR = DATA_DIR / "cleaned"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
